1ed6896714a2b89b212d1bc03d0fc9e11d6d188c
webnetkz <dbqqbq@gmail.com>
2022-01-11 16:54:16 +0600

    update styles

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,22 @@
 "use strict";
 
+function showMenu() {
+    let menu = document.querySelector('menu');
+
+    if(menu.classList.contains('tmpshka')) {
+        menu.style.left = '100vw';
+        menu.classList.remove('tmpshka');
+
+        for(let sect of document.querySelectorAll('section')) {
+            sect.style.filter = 'blur(0)';
+        }
+        document.querySelector('.showM').style.visibility = 'visible';
+    } else {
+        menu.style.left = '0px';
+        menu.classList.add('tmpshka');
+        for(let sect of document.querySelectorAll('section')) {
+            sect.style.filter = 'blur(15px)';
+        }
+        document.querySelector('.showM').style.visibility = 'hidden';
+    }
+}
