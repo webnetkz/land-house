66fbd59e7958d3d38913b190a4ebaf71cc9d806e
webnetkz <dbqqbq@gmail.com>
2022-01-20 16:43:11 +0600

    finish mobile version

--- public/js/main.js
+++ public/js/main.js
@@ -20,3 +20,24 @@ function showMenu() {
         document.querySelector('.showM').style.visibility = 'hidden';
     }
 }
+
+function showCall() {
+    let call = document.querySelector('.call');
+
+    if(call.classList.contains('tmpshka')) {
+        call.style.left = '100vw';
+        call.classList.remove('tmpshka');
+
+        for(let sect of document.querySelectorAll('section')) {
+            sect.style.filter = 'none';
+        }
+        document.querySelector('.showM').style.visibility = 'visible';
+    } else {
+        call.style.left = '0px';
+        call.classList.add('tmpshka');
+        for(let sect of document.querySelectorAll('section')) {
+            sect.style.filter = 'blur(15px)';
+        }
+        document.querySelector('.showM').style.visibility = 'hidden';
+    }
+}
